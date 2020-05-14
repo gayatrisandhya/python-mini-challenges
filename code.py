5ac9019348007bdbc498492a5cbb966be3f48312
gayatrisandhya <gayatrisandhya@users.noreply.github.com>
2020-05-14 00:54:10 -0700

    Code file created.

---
+++ code.py
@@ -0,0 +1,77 @@
+# --------------
+#Code starts here
+
+#Function to check for palindrome
+def palindrome_check(num):
+  num=str(num)
+  return (num[::-1]==num)
+
+#Function to find the smallest palindrome
+def palindrome(num):
+    while(1):
+        num=num+1
+        if palindrome_check(num):
+            return num
+        
+        
+#Code ends here        
+
+
+# --------------
+#Code starts here
+
+#Function to find anagram of one word in another
+
+def a_scramble(str_1,str_2):
+    result=True
+    for i in (str_2.lower()):
+        if i not in (str_1.lower()):
+            result=False
+            break
+        str_1=str_1.replace(i,'',1) #Removing the letters from str_1 that are already checked
+    
+    return (result)
+
+#Code ends here
+
+
+# --------------
+#Code starts here
+import math
+def perfect(x):
+    s=int(math.sqrt(x))
+    return s*s==x
+def check_fib(num):
+    return perfect(5*num*num+4) or perfect(5*num*num-4)
+
+
+# --------------
+#Code starts here
+
+#Function to compress string
+def compress(word):
+    word=word.lower()
+    mist=[]
+    l=0
+    while(l<len(word)):
+        m=word[l]
+        j=0
+        while(l<len(word) and word[l]==m):
+                 j=j+1
+                 l=l+1    
+
+        mist.append(m)
+        mist.append(str(j))
+    
+    return ''.join(mist)
+
+#Code ends here
+
+
+# --------------
+def k_distinct(string,k):
+    s_list=(set(string.lower()))
+    return len(s_list)>=k
+
+
+
